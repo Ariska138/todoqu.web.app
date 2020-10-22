a1fe46e0de722ea712a6fa0088045ad32bfc9525
Ariska Hidayat <ariska138@gmail.com>
2020-10-22 11:46:53 +0700

    change exact to component switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import SignIn from "./components/auth/SignIn"
 import SignUp from './components/auth/SignUp';
 import Dashboard from './components/dashboard/Dashboard';
@@ -8,10 +8,12 @@ function App() {
   return (
     <BrowserRouter>
     <h3 className="text-primary">TodoQu</h3>
+    <Switch>
     <Route path="/signin" component={SignIn}/>
     <Route path="/signup" component={SignUp}/>
-    <Route path="/" exact component={Dashboard}/>
+    <Route path="/" component={Dashboard}/>
     
+    </Switch>
     </BrowserRouter>
    
   );
