d421d3eb921706023cd43b99371793f0506f21a9
Ariska Hidayat <ariska138@gmail.com>
2020-10-22 14:07:26 +0700

    set config firebase

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,20 @@
+ import firebase from "firebase/app";
+ import "firebase/firestore";
+import "firebase/auth";
+ 
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyCrbDjDaa1T7KiXggar16mHLid96Haw4zo",
+    authDomain: "todoqu.firebaseapp.com",
+    databaseURL: "https://todoqu.firebaseio.com",
+    projectId: "todoqu",
+    storageBucket: "todoqu.appspot.com",
+    messagingSenderId: "135932337458",
+    appId: "1:135932337458:web:e5938be044bb4e6c444b2b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  firebaseConfig.firestore();
+
+  export default firebase;
