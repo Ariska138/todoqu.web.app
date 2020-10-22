3d3d80fff2591960b6473bde60fadecdc4fc058d
Ariska Hidayat <ariska138@gmail.com>
2020-10-22 11:42:27 +0700

    make route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
+import {BrowserRouter, Route} from "react-router-dom";
+import SignIn from "./components/auth/SignIn"
+import SignUp from './components/auth/SignUp';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   return (
-   <h3 className="text-primary">TodoQu</h3>
+    <BrowserRouter>
+    <h3 className="text-primary">TodoQu</h3>
+    <Route path="/signin" component={SignIn}/>
+    <Route path="/signup" component={SignUp}/>
+    <Route path="/" component={Dashboard}/>
+    
+    </BrowserRouter>
+   
   );
 }
 
