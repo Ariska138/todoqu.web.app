3a361942f4f658f63cf08eef8d437f78bb4a0c5f
Ariska Hidayat <ariska138@gmail.com>
2020-10-22 14:26:00 +0700

    setup reducer firebase

--- src/Reducers/rootReducer.js
+++ src/Reducers/rootReducer.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { combineReducers } from 'redux';
+import {FirebaseReducer, firebaseReducer} from "react-redux-firebase";
+import {firestoreReducer} from "redux-firestore";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+    firebase: FirebaseReducer,
+    firestore: firestoreReducer
+});
 
 export default rootReducer;
--- src/index.js
+++ src/index.js
@@ -4,17 +4,32 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import "bootstrap/dist/css/bootstrap.css";
+//redux
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './Reducers/rootReducer';
 import {Provider} from 'react-redux';
 import thunk from "redux-thunk";
+//firebase
+import {getFirebase, ReactReduxFirebaseProvider} from "react-redux-firebase";
+import firebaseConfig from "./config/firebaseConfig";
+import {createFirestoreInstance} from "redux-firestore";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(thunk.withExtraArgument({getFirebase})));
+
+//react-redux-firebase props
+const rrfProps = {
+  firebaseConfig,
+  config: {},
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store = {store}>
-    <App />
+      <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+      </ReactReduxFirebaseProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
