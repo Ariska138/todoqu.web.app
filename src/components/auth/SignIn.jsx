bc463021f20edd33fd61500e101167a3be632c13
Ariska Hidayat <ariska138@gmail.com>
2020-10-22 12:38:25 +0700

    modify page sign in from bootstrap form

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,14 +1,28 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class SignIn extends Component {
+  state = {};
 
-    state = {};
-
-    render() {
-        return (
-            <h1> Sign In</h1>
-        );
-    }
+  render() {
+    return (
+      <div className="container my-5">
+        <h1> Sign In</h1>
+        <form autoComplete="off">
+          <div className="form-group">
+            <label htmlFor="email">Email address</label>
+            <input type="email" className="form-control" id="email" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" className="form-control" id="password" />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Sign In
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
