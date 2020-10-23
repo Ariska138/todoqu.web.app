7805e82d8993bb60e175fa1d0432c12275c83a18
Ariska Hidayat <ariska138@gmail.com>
2020-10-23 15:40:48 +0700

    Redirect to login after sign out

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import AddTask from "../tasks/AddTask";
 import Tasks from "../tasks/Tasks";
+import{connect} from "react-redux";
+import { Redirect } from "react-router-dom";
 
-const Dashboard = () => {
+const Dashboard = ({uid}) => {
+  if(!uid) return <Redirect to="/signin"/>
+
   return (
     <div className="container">
       <h3>Dashboard</h3>
@@ -12,4 +16,10 @@ const Dashboard = () => {
   );
 };
 
-export default Dashboard;
+const mapStateProps = (state) => {
+  const uid = state.firebase.auth.uid;
+
+  return {uid};
+}
+
+export default connect(mapStateProps)(Dashboard);
