1968a3e82a46b86c0f247b424cabc9d3e8d190e1
Ariska Hidayat <ariska138@gmail.com>
2020-10-23 09:50:02 +0700

    format date

--- src/components/tasks/Task.jsx
+++ src/components/tasks/Task.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+// format date
+import moment from 'moment';
+
+
 
 const Task = ({task}) => {
     return (
         <tr>
       <th scope="row">{task.task}</th>
-      <td>2020/10/22</td>
+    <td>{moment(task.date.toDate()).calendar()}</td>
       <td><span className="material-icons" style={{cursor:"pointer"}}>
 check_circle</span></td>
       <td><span className="material-icons text-danger" style={{cursor:"pointer"}}>
