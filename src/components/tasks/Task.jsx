6e5d3e58c29c7e33e2be1a1607cf03d418013b97
Ariska Hidayat <ariska138@gmail.com>
2020-10-23 10:44:24 +0700

    formating

--- src/components/tasks/Task.jsx
+++ src/components/tasks/Task.jsx
@@ -1,36 +1,40 @@
-import React from 'react';
+import React from "react";
 // format date
-import moment from 'moment';
-import { removeTask } from '../../actions/taskActions';
-import {connect} from "react-redux";
+import moment from "moment";
+import { removeTask } from "../../actions/taskActions";
+import { connect } from "react-redux";
 
-const Task = ({task, removeTask}) => {
+const Task = ({ task, removeTask }) => {
+  const handleRemove = (task) => {
+    removeTask(task);
+  };
 
-    const handleRemove = task => {
-        removeTask(task);
-    }
-
-    return (
-        <tr>
+  return (
+    <tr>
       <th scope="row">{task.task}</th>
-    <td>{moment(task.date.toDate()).calendar()}</td>
-      <td><span className="material-icons" style={{cursor:"pointer"}}>
-check_circle</span></td>
-      <td><span 
-      className="material-icons text-danger"
-       style={{cursor:"pointer"}}
-       onClick = {() => handleRemove(task)}
-       >
-delete
-</span></td>
+      <td>{moment(task.date.toDate()).calendar()}</td>
+      <td>
+        <span className="material-icons" style={{ cursor: "pointer" }}>
+          check_circle
+        </span>
+      </td>
+      <td>
+        <span
+          className="material-icons text-danger"
+          style={{ cursor: "pointer" }}
+          onClick={() => handleRemove(task)}
+        >
+          delete
+        </span>
+      </td>
     </tr>
-    );
-}
+  );
+};
 
-const mapDispatchToProps = dispatch => {
-    return {
-        removeTask: task => dispatch(removeTask(task)),
-    }
-}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    removeTask: (task) => dispatch(removeTask(task)),
+  };
+};
 
 export default connect(null, mapDispatchToProps)(Task);
