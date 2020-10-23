8cebbc9d485ed8f0472b7be22a8971df9fa9f9be
Ariska Hidayat <ariska138@gmail.com>
2020-10-24 06:25:07 +0700

    add loading

--- src/index.js
+++ src/index.js
@@ -1,38 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 //redux
-import {createStore, applyMiddleware} from 'redux';
-import rootReducer from './Reducers/rootReducer';
-import {Provider} from 'react-redux';
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./Reducers/rootReducer";
+import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 //firebase
-import {getFirebase, ReactReduxFirebaseProvider} from "react-redux-firebase";
+import { getFirebase, ReactReduxFirebaseProvider } from "react-redux-firebase";
 import firebase from "./config/firebaseConfig";
-import {createFirestoreInstance} from "redux-firestore";
+import { createFirestoreInstance } from "redux-firestore";
+//loading
+import { useSelector } from "react-redux";
+import { isLoaded } from "react-redux-firebase";
 
-const store = createStore(rootReducer, applyMiddleware(thunk.withExtraArgument({getFirebase})));
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk.withExtraArgument({ getFirebase }))
+);
 
 //react-redux-firebase props
 const rrfProps = {
   firebase,
   config: {},
   dispatch: store.dispatch,
-  createFirestoreInstance
+  createFirestoreInstance,
+};
+
+function AuthIsLoaded({ children }) {
+  const auth = useSelector((state) => state.firebase.auth);
+  if (!isLoaded(auth))
+    return (
+      <div className="text-center">
+        <div
+          className="spinner-grow text-primary"
+          style={{ width: "7rem", height: "7rem" }}
+          role="status"
+        >
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+    );
+
+  return children;
 }
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store = {store}>
+    <Provider store={store}>
       <ReactReduxFirebaseProvider {...rrfProps}>
-      <App />
+        <AuthIsLoaded>
+          <App />
+        </AuthIsLoaded>
       </ReactReduxFirebaseProvider>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
